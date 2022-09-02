07d15dc9983443c5f687174d419224621153cc95
Tuan Tran <langthamht047@gmail.com>
2022-09-02 08:16:32 -0700

    Call API

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import Pet from "./Pet";
 
 
 const ANIMALS = ["bird", "cats", "dog", "rabbit", "reptile"];
@@ -6,8 +7,20 @@ const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed,setBreed] =useState("");
+  const [pets,setPets] = useState([]);
   const breeds =[];
 
+  useEffect(() =>{
+     requestPets()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
+
+  async function requestPets(){
+      const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`)
+      const json = await res.json();
+      setPets(json.pets);
+  }
+  
   const handleLocation = (e) => {
     setLocation(e.target.value);
   };
@@ -71,6 +84,9 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) =>(
+          <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+      ))}
     </div>
   );
 };
